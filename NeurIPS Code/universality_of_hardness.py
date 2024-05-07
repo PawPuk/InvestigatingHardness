8c039dd1357383236f6b863fb42a7929d9a970cc
PawPuk <ppukowski1@sheffield.ac.uk>
2024-05-07 22:19:40 +0100

    Current version of universality_of_hardness.py
    It finds the 10 samples from each set type that are the hardest according to the networks. After that it shows the threshold-accuracy graph. In next iteration I plan to modify the code responsible for thresholds. Currently, it uses requires the samples to overlap across all networks, which is suboptimal (doesn't show the whole picture and slightly downplays/overplays the results).

--- NeurIPS Code/universality_of_hardness.py
+++ NeurIPS Code/universality_of_hardness.py
@@ -1,9 +1,12 @@
+from typing import List
+
+import matplotlib.pyplot as plt
+import numpy as np
 import torch
+from torch.utils.data import DataLoader
 import torchvision
 import torchvision.transforms as transforms
-from torch.utils.data import DataLoader
 from tqdm import tqdm
-import matplotlib.pyplot as plt
 
 
 def load_cifar10(batch_size=1000):
@@ -18,7 +21,7 @@ def load_cifar10(batch_size=1000):
     return trainset, testset, trainloader, testloader
 
 
-def get_confidence_scores(model, dataloader, device):
+def get_confidence_scores(model, dataloader, device) -> List:
     model.eval()
     confidences = []
     with torch.no_grad():
@@ -29,6 +32,19 @@ def get_confidence_scores(model, dataloader, device):
             top_probs = probs.max(dim=1).values
             confidences.extend(top_probs.cpu().numpy())
     return confidences
+
+
+def plot_samples(data, labels, title):
+    class_names = ['plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+    fig, axes = plt.subplots(1, 10, figsize=(20, 2))
+    for i, ax in enumerate(axes):
+        ax.imshow(data[i].permute(1, 2, 0) * torch.tensor([0.247, 0.243, 0.261]) +
+                  torch.tensor([0.4914, 0.4822, 0.4465]))
+        ax.title.set_text(f'{class_names[labels[i]]}')  # Use class names instead of label numbers
+        ax.axis('off')
+    plt.savefig(f'{title}.png')
+    plt.savefig(f'{title}.pdf')
+    plt.show()
 
 
 def main():
@@ -50,12 +66,43 @@ def main():
     for model_name in models:
         model = torch.hub.load("chenyaofo/pytorch-cifar-models", f"cifar10_{model_name}", pretrained=True)
         model.to(device)
-        model_confidences['train'].append(get_confidence_scores(model, trainloader, device))
-        model_confidences['test'].append(get_confidence_scores(model, testloader, device))
-        combined_loader = DataLoader(trainset + testset, batch_size=128, shuffle=False)
-        model_confidences['combined'].append(get_confidence_scores(model, combined_loader, device))
+        train_confidences = get_confidence_scores(model, trainloader, device)
+        test_confidences = get_confidence_scores(model, testloader, device)
+        combined_loader = DataLoader(trainset + testset, batch_size=1000, shuffle=False)
+        combined_confidences = get_confidence_scores(model, combined_loader, device)
 
-    thresholds = range(1, 101, 1)  # from 2% to 20% in steps of 2%
+        # Convert confidences to numpy arrays
+        model_confidences['train'].append(np.array(train_confidences))
+        model_confidences['test'].append(np.array(test_confidences))
+        model_confidences['combined'].append(np.array(combined_confidences))
+
+    # Find the hardest samples and plot them
+    for key in ['train', 'test', 'combined']:
+        num_samples = int(len(trainset) * 0.03) if key == 'train' else int(len(testset) * 0.03) if key == 'test' \
+            else int((len(trainset) + len(testset)) * 0.03)
+        low_conf_sets = [set(torch.topk(torch.tensor(confidences), num_samples, largest=False).indices.numpy()) for
+                         confidences in model_confidences[key]]
+        common_indices = list(set.intersection(*low_conf_sets))
+        common_indices_np = np.array(common_indices)
+        # Calculate average confidences for common indices
+        average_confidences = np.mean([model_confidences[key][i][common_indices_np] for i in range(len(models))],
+                                      axis=0)
+        # Get indices of the lowest average confidences
+        sorted_indices = np.argsort(average_confidences)  # This sorts from lowest to highest
+        selected_samples = sorted_indices[:10]  # Select the indices of the 10 lowest average confidences
+        selected_indices = [common_indices[i] for i in selected_samples]  # Map back to the original dataset indices
+
+        # Collect the images and labels of the selected indices
+        if key != 'combined':
+            images = [trainset[i][0] if key == 'train' else testset[i][0] for i in selected_indices]
+            labels = [trainset[i][1] if key == 'train' else testset[i][1] for i in selected_indices]
+        else:
+            # Correct handling for combined set
+            images = [trainset[i][0] if i < len(trainset) else testset[i - len(trainset)][0] for i in selected_indices]
+            labels = [trainset[i][1] if i < len(trainset) else testset[i - len(trainset)][1] for i in selected_indices]
+        plot_samples(images, labels, f'{key}_low_confidence')
+
+    thresholds = range(1, 101, 1)
     overlaps = {'train': [], 'test': [], 'combined': []}
 
     for pct in tqdm(thresholds):
@@ -77,7 +124,7 @@ def main():
     plt.figure(figsize=(12, 8))
     for i, key in enumerate(['train', 'test', 'combined'], 1):
         plt.subplot(1, 3, i)
-        plt.plot(thresholds, overlaps[key], marker='o')
+        plt.plot(thresholds, overlaps[key])
         plt.title(f'{key.capitalize()} Set Overlap')
         plt.xlabel('Threshold (%)')
         plt.ylabel('Overlap (%)')
