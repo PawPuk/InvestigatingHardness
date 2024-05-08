23b9743e5d4363e57e7d48a23f5c03ba17127665
PawPuk <ppukowski1@sheffield.ac.uk>
2024-05-08 12:01:14 +0100

    Newest version of universality_of_hardness.py
    It's ok, but the issue is that the definition for miodel overlap is wobly resulting in weird plots that might be too hajrd to explain

--- NeurIPS Code/universality_of_hardness.py
+++ NeurIPS Code/universality_of_hardness.py
@@ -78,32 +78,30 @@ def main():
 
     # Find the hardest samples and plot them
     for key in ['train', 'test', 'combined']:
-        num_samples = int(len(trainset) * 0.03) if key == 'train' else int(len(testset) * 0.03) if key == 'test' \
-            else int((len(trainset) + len(testset)) * 0.03)
-        low_conf_sets = [set(torch.topk(torch.tensor(confidences), num_samples, largest=False).indices.numpy()) for
-                         confidences in model_confidences[key]]
-        common_indices = list(set.intersection(*low_conf_sets))
-        common_indices_np = np.array(common_indices)
-        # Calculate average confidences for common indices
-        average_confidences = np.mean([model_confidences[key][i][common_indices_np] for i in range(len(models))],
-                                      axis=0)
+        # Average confidences across all models for each sample
+        all_confidences = np.stack([confidences for confidences in model_confidences[key]])
+        average_confidences = np.mean(all_confidences, axis=0)  # Calculate mean across models (column-wise)
+
         # Get indices of the lowest average confidences
         sorted_indices = np.argsort(average_confidences)  # This sorts from lowest to highest
         selected_samples = sorted_indices[:10]  # Select the indices of the 10 lowest average confidences
-        selected_indices = [common_indices[i] for i in selected_samples]  # Map back to the original dataset indices
 
         # Collect the images and labels of the selected indices
         if key != 'combined':
-            images = [trainset[i][0] if key == 'train' else testset[i][0] for i in selected_indices]
-            labels = [trainset[i][1] if key == 'train' else testset[i][1] for i in selected_indices]
+            images = [trainset[i][0] if key == 'train' else testset[i][0] for i in selected_samples]
+            labels = [trainset[i][1] if key == 'train' else testset[i][1] for i in selected_samples]
         else:
             # Correct handling for combined set
-            images = [trainset[i][0] if i < len(trainset) else testset[i - len(trainset)][0] for i in selected_indices]
-            labels = [trainset[i][1] if i < len(trainset) else testset[i - len(trainset)][1] for i in selected_indices]
+            images = [trainset[i][0] if i < len(trainset) else testset[i - len(trainset)][0] for i in selected_samples]
+            labels = [trainset[i][1] if i < len(trainset) else testset[i - len(trainset)][1] for i in selected_samples]
+
         plot_samples(images, labels, f'{key}_low_confidence')
 
     thresholds = range(1, 101, 1)
-    overlaps = {'train': [], 'test': [], 'combined': []}
+    consensus_thresholds = range(3, 23, 4)  # Starting at 3 and increasing in steps of 4 up to 19
+    overlaps = {'train': {c: [] for c in consensus_thresholds},
+                'test': {c: [] for c in consensus_thresholds},
+                'combined': {c: [] for c in consensus_thresholds}}
 
     for pct in tqdm(thresholds):
         num_train = int(len(trainset) * pct / 100)
@@ -117,18 +115,27 @@ def main():
                                      num_train if key == 'train' else num_test if key == 'test' else num_combined,
                                      largest=False).indices
                 low_conf_sets.append(set(indices.numpy()))
-            overlap = len(set.intersection(*low_conf_sets)) / (
-                num_train if key == 'train' else num_test if key == 'test' else num_combined) * 100
-            overlaps[key].append(overlap)
+            dataset_size = len(trainset) if key == 'train' else len(testset) if key == 'test' else \
+                len(trainset) + len(testset)
+            overlap_count = np.zeros(dataset_size)
+            for c_threshold in consensus_thresholds:
+                overlap_count.fill(0)
+                for lc_set in low_conf_sets:
+                    overlap_count[list(lc_set)] += 1
+                overlap_size = num_train if key == 'train' else num_test if key == 'test' else num_combined
+                overlap1 = (np.sum(overlap_count >= c_threshold) / overlap_size) * 100
+                overlaps[key][c_threshold].append(overlap1)
 
     plt.figure(figsize=(12, 8))
-    for i, key in enumerate(['train', 'test', 'combined'], 1):
-        plt.subplot(1, 3, i)
-        plt.plot(thresholds, overlaps[key])
+    for i, key in enumerate(['train', 'test', 'combined']):
+        plt.subplot(1, 3, i + 1)
+        for c_threshold in consensus_thresholds:
+            plt.plot(thresholds, overlaps[key][c_threshold], label=f'Min {c_threshold} Networks')
         plt.title(f'{key.capitalize()} Set Overlap')
         plt.xlabel('Threshold (%)')
         plt.ylabel('Overlap (%)')
         plt.grid(True)
+        plt.legend()
     plt.tight_layout()
     plt.savefig('universality_of_hardness.pdf')
     plt.savefig('universality_of_hardness.png')
